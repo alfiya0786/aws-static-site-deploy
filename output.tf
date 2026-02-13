fa00d6367c93194cbd00fbf75daa8b5202dc2630
alfiya0786 <alfiyakapadia07@gmail.com>
2026-02-13 12:30:35 +0530

    file add

--- output.tf
+++ output.tf
@@ -2,3 +2,7 @@ output "website_url" {
   description = "The URL of the static website"
   value       = "https://${aws_cloudfront_distribution.s3_distribution.domain_name}"
 }
+
+output "cf_distribution_id" {
+  value = aws_cloudfront_distribution.s3_distribution.id
+}
